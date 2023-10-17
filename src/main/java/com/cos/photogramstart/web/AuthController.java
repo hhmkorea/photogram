3861c6dda98d1dc25642dd3b21c3f6f3599504cf
hhmkorea <yuyee81@gmail.com>
2023-10-17 23:38:24 +0900

    init

---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,34 @@
+package com.cos.photogramstart.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
+@Controller // 1. Ioc 등록 2. 파일을 리턴하는 컨트롤러
+public class AuthController {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+	@GetMapping("/auth/signin")
+	public String signinForm() {
+		return "auth/signin";
+	}
+
+	@GetMapping("/auth/signup")
+	public String signupForm() {
+		return "auth/signup";
+	}
+	
+	// 회원가입버튼 -> /auth/signup -> /auth/signin
+	// 회원가입버튼 X
+	@PostMapping("/auth/signup")
+	public String signup(SignupDto signupDto) { // 회원가입, key=value (x-www-form-urlencoded)
+		log.info(signupDto.toString());
+		return "auth/signin"; // 로그인
+	}
+}
