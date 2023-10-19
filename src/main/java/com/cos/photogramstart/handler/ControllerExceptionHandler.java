6e2bc0c83c4781a0990c28e239ca0494ef10515e
hhmkorea <yuyee81@gmail.com>
2023-10-19 21:36:51 +0900

    공통 응답 Script 구현

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController // 데이터 리턴 
 @ControllerAdvice // 모든 Excepiton을 낚아챔
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public Map<String, String> validationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public String validationException(CustomValidationException e) {
+		// CMRespDto, Script 비교
+		// 1. 클라이언트에게 응답할 때는 Script가 좋음.
+		// 2. Ajax 통신 - CMRespDto
+		// 3. Android 통신 - CMRespDto
+		return Script.back(e.getErrorMap().toString());
 	}
 }
