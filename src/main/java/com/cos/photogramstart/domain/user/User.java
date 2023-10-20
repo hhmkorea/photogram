3226f346903aea31c695101c39d6715e5efed573
hhmkorea <yuyee81@gmail.com>
2023-10-20 22:08:22 +0900

    long -> int

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class User {	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
-	private Long id;
+	private int id;
 	
 	@Column(length = 20, unique =  true) // 제약조건 
 	private String username;
