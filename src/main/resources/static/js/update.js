b685ad5ecbcb7e209b45b35fc8901e70fef45b78
hhmkorea <yuyee81@gmail.com>
2023-10-20 22:22:46 +0900

    x-www-form-urlencoded

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -6,11 +6,11 @@ function update(userId) {
 	console.log(data);
 	
 	$.ajax({
-		type : "put",
-		url : `/api/user/${userId}`,
-		data : data, 
-		contentType : "application/x-www-form-rulencoded; charset=utf-8",
-		dataType : "json"	
+		type: "put",
+		url: `/api/user/${userId}`,
+		data: data, 
+		contentType: "application/x-www-form-urlencoded; charset=utf-8",
+		dataType: "json"	
 	}).done(res=>{
 		console.log("update 성공");
 	}).fail(error=>{
