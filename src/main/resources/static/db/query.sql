970f5ed1e1995105a24d845c3c46572ead84dd94
hhmkorea <yuyee81@gmail.com>
2023-10-27 22:11:48 +0900

    query.sql

---
+++ src/main/resources/static/db/query.sql
@@ -0,0 +1,20 @@
+CREATE USER 'cos'@'%' IDENTIFIED BY 'cos1234'; 
+GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
+CREATE DATABASE photogram;
+
+SELECT * FROM user; 
+DESC USER;
+TRUNCATE user;
+
+SELECT * FROM subscribe;
+SELECT * FROM image;
+TRUNCATE image;
+
+-- 구독수 
+SELECT COUNT(*) FROM subscribe WHERE fromUserId = 1;
+
+-- 구독여부(ssal로 로그인, cos 페이지로 감) 
+SELECT * FROM subscribe WHERE fromUserId = 1 AND toUserId = 2;
+
+
+
